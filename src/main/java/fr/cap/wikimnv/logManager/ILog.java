b7c878e0a67961e2ce1c5e6d0cb4806ea546b67a
franckyy <franck.calestroupat@gmail.com>
2015-09-12 10:48:53 +0100

    FC *_* nettoyage
    projet RUN_ON_SERVER -> OK, import wsdl _> OK

--- src/main/java/fr/cap/wikimnv/logManager/ILog.java
+++ src/main/java/fr/cap/wikimnv/logManager/ILog.java
@@ -4,6 +4,5 @@ import javax.jws.WebService;
 
 @WebService(name="loggerService", serviceName="loggerService")
 public interface ILog {
-	
 	public String logger(LogLevel logLvl, String message) throws Exception;
 }
